bdf85fb0644de9c3e75fc3cf347bd10ad1007ded
niall-mccloy <niall.mccloy@kainos.com>
2024-08-01 16:30:34 +0100

    populating tables and adding query

--- src/main/resources/db/migration/V9__adding_query_for_clients.sql
+++ src/main/resources/db/migration/V9__adding_query_for_clients.sql
@@ -5,6 +5,21 @@ from Client
          join Project
               using(ClientId);
 
+insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
+values ('Terry John', 60000.00, 10295396939285, 03928690340, 5.0);
+
+insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
+values ('John Clean', 40000.00, 2843905783828, 0795832959, 5.00);
+
+insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
+values ('Dean Smith', 45000.00, 6758473625143, 0694873849, 5.00);
+
+insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
+values ('Clara Sting', 65000.00, 877476375837, 657463527, 9.00);
+
+insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
+values ('Seana Green', 70000.00, 3457342095949, 756473736, 7.00);
+
 insert into Client ( Client_Name, Client_Address, Client_PhoneNumber, SalesEmployee_ID)
 values ('John Mcauley', '2 Granary Drive', '08942467313',4);
 
@@ -21,20 +36,7 @@ insert into Client ( Client_Name, Client_Address, Client_PhoneNumber, SalesEmplo
 values ('Reece Wayne', '57 Strand Road', '0776431963',1);
 
 
-insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
-values ('Terry John', 60000.00, 10295396939285, 03928690340, 15.00,1);
 
-    insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
-values ('John Clean', 40000.00, 2843905783828, 0795832959, 15.00),1;
-
-insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
-values ('Dean Smith', 45000.00, 6758473625143, 0694873849, 15.00,3);
-
-insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
-values ('Clara Sting', 65000.00, 877476375837, 657463527, 20.00,5);
-
-insert into SalesEmployees (SalesEmployee_Name,SalesEmployee_Salary,SalesEmployee_BankAccountNum,SalesEmployee_NationalInsuranceNum,SalesEmployee_CommissionRate)
-values ('Seana Green', 70000.00, 3457342095949, 756473736, 25.00,3);
 
 update Project
 SET ClientId = 1 Where Project_ProjectID = 1;
