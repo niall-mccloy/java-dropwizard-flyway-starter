ada9cd57df51f0f97767030de588e43fc11516f8
TaylorB <taylor.brogan@kainos.com>
2024-08-01 15:45:37 +0100

    Gets Client Project Values

---
+++ src/main/resources/db/migration/V10__client_project_values.sql
@@ -0,0 +1,14 @@
+SELECT Client_Name, SUM(Project_Value) AS `Total Project Value`
+    FROM Projects
+        join Client using (ClientId)
+        GROUP BY Client_Name ORDER BY SUM(Project_Value) DESC LIMIT 1;
+
+SELECT Client_Name, SUM(Project_Value) AS `Total Project Value`
+    FROM Projects
+        join Client using (ClientId)
+        GROUP BY Client_Name ORDER BY SUM(Project_Value) ASC LIMIT 1;
+
+SELECT Client_Name, AVG(Project_Value) AS `Average Project Value`
+    FROM Projects
+        join Client using (ClientId)
+        GROUP BY Client_Name ORDER BY AVG(Project_Value) DESC;
